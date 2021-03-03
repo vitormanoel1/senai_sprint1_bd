bd7b70c2f46aee73f4d736eccea334625026fecb
vitormanoel1 <vitorm4123@gmail.com>
2021-03-02 21:30:50 -0300

    Projeto bd SENAI_HROADS_TARDE Concluido

---
+++ Desafio/HROADS_02_DML.sql
@@ -0,0 +1,47 @@
+----------------DML--------------------
+
+use SENAI_HROADS_TARDE;
+
+INSERT INTO CLASSE(NOME)
+VALUES	('BARBARO'),
+		('CRUZADO'),
+		('CAÇADORA DE DEMONIOS'),
+		('MONGE'),
+		('NECROMANTE'),
+		('FEITICEIRO'),
+		('ARCADISTA');
+
+INSERT INTO TIPOHABILIDADE(NOME)
+VALUES		('ATAQUE'),
+			('DEFESA'),
+			('CURA'),
+			('MAGIA');
+
+INSERT INTO HABILIDADE(IdTIPO,NOME)
+VALUES		(1, 'LANÇA MORTAL'),
+			(2, 'ESCUDO SUPREMO'),
+			(3, 'RECUPERAR VIDA'),
+			(4, 'SEM HABILIDADE');
+
+
+INSERT INTO PERSONAGEM(IdCLASSE, NOME, MAXIMOVIDA, MAXIMOVIDAMANA, DATATUALIZACAO,DATACRIACAO)
+VALUES			(1, 'DEUBUG', 100, 80, GETDATE(), GETDATE()),
+				(4, 'BITBUG', 70, 100, GETDATE(), GETDATE()),
+				(7, 'FER8', 75, 60, GETDATE(), GETDATE());
+
+
+
+
+--Atualizar o nome do personagem Fer8 para Fer7;
+------------------------------------------------
+UPDATE PERSONAGEM
+SET NOME = 'FER7'
+WHERE IdPERSONAGEM = 3;
+
+
+--Atualizar o nome da classe de Necromante para Necromancer;
+-----------------------------------------------------------
+UPDATE CLASSE
+SET NOME = 'NECROMANCER'
+WHERE IdCLASSE = 5;
+
